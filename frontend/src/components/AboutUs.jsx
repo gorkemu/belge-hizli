4420cd822ce8db990d78988dd7dcda0bd536f11e
gorkemu <gorkemunal88@gmail.com>
2025-04-18 08:12:19 +0300

    update aboutus

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -12,8 +12,8 @@ function AboutUs() {
 
       {/* Paragraf stilini uygula */}
       <p className={styles.paragraph}>
-        <strong>{siteName}</strong> olarak amacımız, [amacınızı buraya yazın, örn: sıkça ihtiyaç duyulan yasal belgeleri
-        hazırlama sürecini kolaylaştırmak, hızlı ve pratik çözümler sunmaktır]. Teknolojiyi kullanarak,
+        <strong>{siteName}</strong> olarak amacımız, ıkça ihtiyaç duyulan yasal belgeleri
+        hazırlama sürecini kolaylaştırmak, hızlı ve pratik çözümler sunmaktır. Teknolojiyi kullanarak,
         belirli standartlardaki belge ihtiyaçlarınızı, uzmanlara danışma ihtiyacı duymadan (ancak bunun hukuki tavsiye olmadığını unutmayın!),
         kendi bilgilerinizle hızlıca oluşturmanıza olanak tanıyoruz.
       </p>
